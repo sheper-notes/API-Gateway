85ffa0ae38ab2569bb8f7b27a402abb6f2ee2847
flexiconn <simoncox2003@gmail.com>
2023-12-08 12:49:55 +0100

    Fix authentication

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Ocelot.DependencyInjection;
 using Ocelot.Middleware;
 using Ocelot.Provider.Eureka;
@@ -11,31 +12,54 @@ namespace API_Gateway
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
+            var corsOrigin = builder.Configuration.GetValue<string>("CorsOrigin");
+            var auth0Domain = $"https://{builder.Configuration.GetValue<string>("auth0Domain")}/";
+            var auth0Audience = builder.Configuration.GetValue<string>("auth0Audience");
+            var authenticationProviderKey = "TestKey";
 
+            var corsPolicy = "SheperCorsPolicy";
             // Add services to the container.
 
             builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddAuthentication(options =>
+                {
+                    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+                }
+            ).AddJwtBearer(options =>
+            {
+                options.Authority = auth0Domain;
+                options.Audience = auth0Audience;
+            });
             builder.Services.AddSwaggerGen();
             builder.Configuration.AddJsonFile("Controllers/ocelot.json");
-            builder.Services.AddOcelot().AddKubernetes();
-
+            builder.Services.AddOcelot();
+            builder.Services.AddCors(options =>
+            {
+                options.AddPolicy(name: corsPolicy,
+                    policy =>
+                    {
+                        policy.WithOrigins(corsOrigin).AllowCredentials().AllowAnyHeader().AllowAnyMethod();
+                    });
+            });
             var app = builder.Build();
-
+            app.UseCors(corsPolicy);
+            
             // Configure the HTTP request pipeline.
             //if (app.Environment.IsDevelopment())
             //{
-                app.UseSwagger();
+            app.UseSwagger();
                 app.UseSwaggerUI();
             //}
 
             //app.UseHttpsRedirection();
-
-            //app.UseAuthorization();
+            app.UseAuthentication();
 
             app.UseOcelot().Wait();
             
+
 
             app.MapControllers();
 
