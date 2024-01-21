00f9dc143d9149dec84194b8038de02c6d44e5ea
Simon Cox <simoncox2003@gmail.com>
2024-01-21 23:13:01 +0100

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -12,9 +12,6 @@ namespace API_Gateway
             var corsOrigin = builder.Configuration.GetValue<string>("CorsOrigin");
             var auth0Domain = $"https://{builder.Configuration.GetValue<string>("auth0Domain")}/";
             var auth0Audience = builder.Configuration.GetValue<string>("auth0Audience");
-            auth0Audience = "https://sheper.eu.auth0.com/api/v2/";
-            auth0Domain = "sheper.eu.auth0.com";
-            var authenticationProviderKey = "TestKey";
             Console.WriteLine("Loaded: " + corsOrigin);
             Console.WriteLine("Loaded: " + auth0Domain);
             Console.WriteLine("Loaded: " + auth0Audience);
@@ -25,17 +22,17 @@ namespace API_Gateway
             builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
-            //builder.Services.AddAuthentication(options =>
-            //    {
-            //        options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-            //        options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-            //    }
-            //).AddJwtBearer(options =>
-            //{
-            //    options.Authority = auth0Domain;
-            //    options.Audience = auth0Audience;
-            //    options.RequireHttpsMetadata = false;
-            //});
+            builder.Services.AddAuthentication(options =>
+                {
+                    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+                }
+            ).AddJwtBearer(options =>
+            {
+                options.Authority = auth0Domain;
+                options.Audience = auth0Audience;
+                options.RequireHttpsMetadata = false;
+            });
             builder.Services.AddSwaggerGen();
             builder.Configuration.AddJsonFile("Controllers/ocelot.json");
             builder.Services.AddOcelot();
